eac6e6ab7df05faaaea06d79358a49792a485045
SergiiL <ljore@ukr.net>
2014-09-13 14:41:11 +0300

    Initial commit

---
+++ src/main/java/HappinestWorld/App.java
@@ -0,0 +1,13 @@
+package HappinestWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
